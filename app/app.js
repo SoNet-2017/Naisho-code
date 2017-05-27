d906966150715de5fb5e8f0675c9d9ad14c15ea6
MicheleDrago <s189127@studenti.polito.it>
2017-05-27 20:39:15 +0200

    lb

---
+++ app/app.js
@@ -0,0 +1,55 @@
+'use strict';
+// Initialize the Firebase SDK
+var config = {
+    apiKey: "AIzaSyB25qzSL59ESy04SBFhwbXJVdCjEKEFapc",
+    authDomain: "naisho-124b5.firebaseapp.com",
+    databaseURL: "https://naisho-124b5.firebaseio.com",
+    projectId: "naisho-124b5",
+    storageBucket: "naisho-124b5.appspot.com",
+    messagingSenderId: "511020877316"
+};
+firebase.initializeApp(config);
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+    "firebase",
+    'ngRoute',
+    'ngMap',
+    'myApp.detailsView',
+    'myApp.loginView',
+    'myApp.authentication',
+    'myApp.users',
+    'myApp.usersListView',
+    'myApp.chatView',
+    'myApp.userProfileView',
+    'myApp.userRegistrationView',
+    'myApp.fileUpload'
+])
+    .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+        $locationProvider.hashPrefix('!');
+        $routeProvider.otherwise({redirectTo: ''});
+    }])
+    .run(["$rootScope", "$location", function($rootScope, $location) {
+        $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+            // We can catch the error thrown when the $requireSignIn promise is rejected
+            // and redirect the user back to the home page
+            if (error === "AUTH_REQUIRED") {
+                $location.path("/loginView");
+            }
+        });
+    }])
+    .controller('MainCtrl', ['$scope', '$rootScope', '$firebaseAuth', function($scope, $rootScope, $firebaseAuth) {
+        //this controller only declares a function to get information about the user status (logged in / out)
+        //it is used to show menu buttons only when the user is logged
+
+        //set the variable that is used in the main template to show the active button
+        $rootScope.dati = {};
+        $rootScope.dati.currentView = 'home';
+        $scope.isLogged = function()
+        {
+            if ($firebaseAuth().$getAuth())
+                return true;
+            else
+                return false;
+        }
+    }]);
--- app/userRegistrationView/userRegistrationView.js
+++ app/userRegistrationView/userRegistrationView.js
@@ -17,16 +17,16 @@ angular.module('myApp.userRegistrationView', ['ngRoute'])
         //check if the second password is equal to the first one
         if ($scope.user.password!= '' && $scope.user.password === $scope.user.password2) {
             //create a new user with specified email and password
-            Auth.$createUserWithEmailAndPassword($scope.user.email, $scope.user.password)
+            Auth.$createUserWithEmailAndPassword($scope.user.email, $scope.user.password, $scope.user.sesso,$scope.user.DatadiNascita,$scope.user.Buddista)
                 .then(function (firebaseUser) {
                     //after creating the user, we will perform a login and then the new information will be saved in the database
                     //(the reason is that we cannot write in the database if we are not logged in ... it is not the best way of doing it but it is ok for our prototype)
                     Auth.$signInWithEmailAndPassword($scope.user.email, $scope.user.password).then(function(internalFirebaseUser) {
                         var userId = internalFirebaseUser.uid;
-                        Users.registerNewUserInfo(userId, $scope.user.name, $scope.user.surname, $scope.user.email);
+                        Users.registerNewUserInfo(userId, $scope.user.name, $scope.user.surname, $scope.user.email,$scope.user.sesso,$scope.user.DatadiNascita,$scope.user.Buddista);
                         Users.registerLogin(userId, $scope.user.email);
                         // login successful: redirect to the pizza list
-                        $location.path("/pizzaView");
+                        $location.path("");
                     }).catch(function(error) {
                         $scope.error = error;
                         console.log(error.message);
