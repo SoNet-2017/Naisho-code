4aa90e8e6996a1a1d6bebd9b826f47df058be3c9
MicheleDrago <s189127@studenti.polito.it>
2017-06-01 13:40:52 +0200

    un pò di codice per database firebase

--- app/homeView/homeView.js
+++ app/homeView/homeView.js
@@ -5,7 +5,7 @@ angular.module('myApp.homeView', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/homeView', {
     templateUrl: 'homeView/homeView.html',
-    controller: 'View1Ctrl',
+    controller: 'homeCtrl',
       resolve: {
           // controller will not be loaded until $requireSignIn resolves
           // Auth refers to our $firebaseAuth wrapper in the factory below
@@ -18,8 +18,18 @@ angular.module('myApp.homeView', ['ngRoute'])
       }
   })
 }])
+    .controller('homePageCtrl', ['$scope', 'Frase','$location', 'Auth', '$firebaseObject','Users', 'currentAuth', '$firebaseAuth', function ($scope,Frase,$location, Auth, $firebaseObject, Users, currentAuth, $firebaseAuth) {
+        $scope.dati={};
+        $scope.auth=Auth;
 
-    .controller('View1Ctrl','$scope', 'Frase', function($scope, Frase) {
+        $scope.showSearchItem=function () {
+            var x = document.getElementById("typeSearchContentHome");
+            if (x.className.indexOf("w3-show") == -1)
+                x.className += " w3-show";
+            else
+                x.className = x.className.replace(" w3-show", "");
+        };
+
         var ref = firebase.database().ref("frasi/frase");
         ref.once("value")
             .then(function(snapshot) {
@@ -37,8 +47,21 @@ angular.module('myApp.homeView', ['ngRoute'])
             });
     });
 
+        $scope.logout = function () {
+            Users.registerLogout(currentAuth.uid);
+            $firebaseAuth().$signOut();
+            $firebaseAuth().$onAuthStateChanged(function(firebaseUser) {
+                if (firebaseUser) {
+                    console.log("User is yet signed in as:", firebaseUser.uid);
+                } else {
+                    $location.path("/loginView");
+                }
+            });
 
-       /* //noinspection JSAnnotator
+
+        };
+
+          /* //noinspection JSAnnotator
         $scope.shownav-bar=function () {
             var x = document.getElementById("topBarHome");
             if (x.className.indexOf("w3-show") == -1)
@@ -48,3 +71,4 @@ angular.module('myApp.homeView', ['ngRoute'])
         };
 
 */
+    }]);
