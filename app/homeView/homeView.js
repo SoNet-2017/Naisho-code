7efd9cad0a65026470b92b8c8c3d6f40b86ee88f
Selene <s239776@studenti.polito.it>
2017-06-01 12:49:24 +0200

    1giugno

--- app/homeView/homeView.js
+++ app/homeView/homeView.js
@@ -5,7 +5,7 @@ angular.module('myApp.homeView', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/homeView', {
     templateUrl: 'homeView/homeView.html',
-    controller: 'View1Ctrl',
+    controller: 'homeCtrl',
       resolve: {
           // controller will not be loaded until $requireSignIn resolves
           // Auth refers to our $firebaseAuth wrapper in the factory below
@@ -18,14 +18,35 @@ angular.module('myApp.homeView', ['ngRoute'])
       }
   })
 }])
+    .controller('homePageCtrl', ['$scope', 'Frase','$location', 'Auth', '$firebaseObject','Users', 'currentAuth', '$firebaseAuth', function ($scope,Frase,$location, Auth, $firebaseObject, Users, currentAuth, $firebaseAuth) {
+        $scope.dati={};
+        $scope.auth=Auth;
 
-    .controller('View1Ctrl', ['$scope','Frase',function($scope, Frase) {
-            $scope.dati={};
-            $scope.dati.frase = Frase.getData();
+        $scope.showSearchItem=function () {
+            var x = document.getElementById("typeSearchContentHome");
+            if (x.className.indexOf("w3-show") == -1)
+                x.className += " w3-show";
+            else
+                x.className = x.className.replace(" w3-show", "");
+        };
 
 
 
-       /* //noinspection JSAnnotator
+        $scope.logout = function () {
+            Users.registerLogout(currentAuth.uid);
+            $firebaseAuth().$signOut();
+            $firebaseAuth().$onAuthStateChanged(function(firebaseUser) {
+                if (firebaseUser) {
+                    console.log("User is yet signed in as:", firebaseUser.uid);
+                } else {
+                    $location.path("/loginView");
+                }
+            });
+
+
+        };
+
+          /* //noinspection JSAnnotator
         $scope.shownav-bar=function () {
             var x = document.getElementById("topBarHome");
             if (x.className.indexOf("w3-show") == -1)
