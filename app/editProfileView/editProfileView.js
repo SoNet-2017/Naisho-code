0c7b1c84b24d0d7d7583ef9c70fcb475cfd49428
Selene <s239776@studenti.polito.it>
2017-06-03 15:31:30 +0200

    cartella editProfile

---
+++ app/editProfileView/editProfileView.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('myApp.userRegistrationView', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/userRegistration', {
+    templateUrl: 'userRegistrationView/userRegistrationView.html',
+    controller: 'UserRegistrationCtrl'
+  });
+}])
+
+.controller('UserRegistrationCtrl', ['$scope', '$rootScope', 'Auth', 'Users', '$location', function($scope, $rootScope, Auth, Users, $location) {
+    $scope.user={};
+
+    $scope.signUp = function() {
+        if ($scope.user.password!= '' && $scope.user.password === $scope.user.password2) {
+            Auth.$createUserWithEmailAndPassword($scope.user.email, $scope.user.password)
+                .then(function (firebaseUser) {
+                    Auth.$signInWithEmailAndPassword($scope.user.email, $scope.user.password).then(function(internalFirebaseUser) {
+                        var userId = internalFirebaseUser.uid;
+                        Users.registerNewUserInfo(userId, $scope.user.name, $scope.user.surname, $scope.user.email,$scope.user.type);
+                        Users.registerLogin(userId, $scope.user.email);
+                        // login successful: redirect to the home
+                        $location.path("/homeView");
+                    }).catch(function(error) {
+                        $scope.error = error;
+                        console.log(error.message);
+                    });
+                }).catch(function (error) {
+                    $scope.error = error;
+                    console.log(error.message);
+            });
+        }
+    };
+}]);
