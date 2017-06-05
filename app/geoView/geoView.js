24c9adb28bd5ab8b117fbb893c5f11bb5c090277
MicheleDrago <s189127@studenti.polito.it>
2017-06-05 20:20:32 +0200

    geo

--- app/geoView/geoView.js
+++ app/geoView/geoView.js
@@ -17,23 +17,28 @@ angular.module('myApp.mapView', ['ngRoute'])
 
             }
         });
-    }])
-    .controller('geoViewCtrl', ['$scope','$rootScope', function($scope,$rootScope) {
+    }]);
+
+
+    .controller('geoViewCtrl',['$scope', function($scope) {
         //initialize variables
         $scope.dati={};
         $scope.dati.vm = this;
         $scope.dati.vm.positions = [];
+
         //set the variable that is used in the main template to show the active button
-        $rootScope.dati.currentView = "geo";
+        //$rootScope.dati.currentView = "geo";
         //get the list of available pizzas
         //$scope.dati.pizzas = Pizza.getData();
+
         $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyD6qAQOEvZs2XlUUu3ziu-nrDX-WWZXap4";
         //only when all data will be loaded, the map will be created
-        $scope.dati.then(function () {
+        $scope.dati=function () {
                 var lat = 45.071087;
                 var lng = 7.686567;
                 $scope.dati.vm.positions.push({lat: lat, lng: lng});
-            });
 
-    }]);
+            }];
+
+    })
 
