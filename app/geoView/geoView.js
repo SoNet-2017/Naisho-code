dd245e06dffb5539836e1078bb2ac82f5ad94bc8
MicheleDrago <s189127@studenti.polito.it>
2017-06-17 19:24:27 +0200

    se un utente è online si vede il marker nella mappa

--- app/geoView/geoView.js
+++ app/geoView/geoView.js
@@ -19,18 +19,22 @@ angular.module('myApp.geoView', ['ngRoute'])
         });
     }])
 
-    .controller('geoViewCtrl', ['$scope', '$rootScope',
-        function($scope, $rootScope){
+    .controller('geoViewCtrl', ['$scope', '$rootScope','$firebaseAuth','$routeParams','UserList',
+        function($scope, $rootScope, $firebaseAuth, $routeParams, UserList){
 //initialize variables
             $scope.dati = {};
             $scope.address= {};
             $scope.pos = {};
+            //$scope.userId = {};
+            $scope.coordinate = {};
+            $scope.lat = {};
+            $scope.lng = {};
 
 
 
 //set the variable that is used in the main template to show the active button
             $rootScope.dati.currentView = "geoView";
-            //var userId = $firebaseAuth().$getAuth().uid;
+
             $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyD6qAQOEvZs2XlUUu3ziu-nrDX-WWZXap4";
             $scope.geo = navigator.geolocation.getCurrentPosition(function(position) {
 
@@ -43,6 +47,35 @@ angular.module('myApp.geoView', ['ngRoute'])
                // console.log("coordinate utente da geoview:", $scope.address);
 
             });
+
+            $scope.dati.listaUtenti = UserList.getListOfUsers();
+            $scope.dati.userId = $firebaseAuth().$getAuth().uid;
+            $scope.dati.listaUtenti.$loaded().then(function () {
+                for (var i=0;i<$scope.dati.listaUtenti.length; i++){
+                    if ($scope.dati.userId!== $scope.dati.listaUtenti[i].$id && $scope.dati.listaUtenti[i].logged===true){
+                        //for (var b=0;i<($scope.dati.listaUtenti.length*2); b=+2){
+                        $scope.lat=$scope.dati.listaUtenti[i].address.lat;
+                        $scope.lng=$scope.dati.listaUtenti[i].address.lng;
+
+                console.log("lat e lng di tutti", $scope.lat, $scope.lng);
+                //$scope.dati.vm.positions.push({address: address});
+                    //}
+                }}});
+
+           // console.log("vediamo cosa è dati.listaUtenti:",  $scope.dati.listaUtenti);
+           // var c=$scope.listaUtenti.$keyAt($scope.listaUtenti[1]);
+          //  console.log("quanto vale $scope.listaUtenti.length", c);
+
+          // for (var i=0;i<$scope.listaUtenti.length; i++){
+            //   console.log("quanto vale $scope.dati.availableUsers.length", $scope.dati.availableUsers.length);
+            //    if ($scope.userId!== $scope.dati.availableUsers.$keyAt(i).$id) {
+            //        console.log("siamo dentro l'if");
+             //      $scope.lat[i]=$scope.dati.availableUsers.$keyAt(i).address.lat;
+             //      $scope.lng[i]=$scope.dati.availableUsers.$keyAt(i).address.lng;
+             //      console.log("vediamo cosa salva il for:",  $scope.lat[i],$scope.lng[i]);
+
+              // }
+           // }
 
         }]);
 
