9b9615196c1a959c1dc8d8cbab63cb9d9c425b4e
Selene <s239776@studenti.polito.it>
2017-06-18 19:41:32 +0200

    modifiche home+ profilo altri utenti/richiste amicizia e tutor non ancora funzionanti

---
+++ app/OtherUserProfile/OtherUserProfile.js
@@ -0,0 +1,75 @@
+'use strict';
+
+angular.module('myApp.OtherUserProfile', ['ngRoute','myApp.users','myApp.post'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/OtherUser/:otherUserId', {
+            templateUrl: 'OtherUserProfile/OtherUserProfile.html',
+            controller: 'OtherUserProfileCtrl',
+            resolve: {
+                // controller will not be loaded until $requireSignIn resolves
+                // Auth refers to our $firebaseAuth wrapper in the factory below
+                "currentAuth": ["Auth", function(Auth) {
+                    // $requireSignIn returns a promise so the resolve waits for it to complete
+                    // If the promise is rejected, it will throw a $routeChangeError (see above)
+                    return Auth.$requireSignIn();
+                }]
+
+            }
+        })
+    }])
+
+   .controller('OtherUserProfileCtrl',['$scope','$rootScope','$routeParams','currentAuth','UsersFriendsService','UsersTutorService', 'UserList',
+   function ($scope, $rootScope, $routeParams, currentAuth, UsersFriendsService,UsersTutorService,  UserList) {
+
+       $scope.dati = {};
+
+       $scope.dati.userId = UsersFriendsService.getUserInfo(currentAuth.uid);
+       $scope.dati.otherUserId = $routeParams.otherUserId;
+
+
+       $scope.dati.recipient = UsersFriendsService.getUserInfo($scope.dati.otherUserId);
+
+       // dati amicizie
+       $scope.dati.friends = UsersFriendsService.getFriends();
+       // dati tutor
+       $scope.dati.tutors = UsersTutorService.getTutors();
+// creazione amicizia
+       $scope.CreateFriendship = function() {
+           UsersFriendsService.insertNewUsersFriendship($scope.dati.userId,$routeParams.otherUserId,'Bottone disabilitato').then(function (ref) {
+               var firendshipId = ref.key;
+               UsersFriendsService.updateUsersFollow(friendshipId);
+               $('#friendsButton').attr('disabled',true);
+           });
+       };
+        $scope.removeFriendship = function (friendshipId) {
+            UsersFriendsService.deleteFriend(friendshipId);
+            $('#friendsButton').prop("disabled",false);
+        };
+
+
+       $scope.CreateTutor = function() {
+           UsersTutorService.insertNewUsersTutor($scope.dati.userId,$routeParams.otherUserId,'Bottone disabilitato').then(function (ref) {
+               var followId = ref.key;
+               UsersTutorService.updateUsersTutor(tutorId);
+               $('#tutorButton').attr('disabled',true);
+           });
+       };
+
+       $scope.CreateTutored = function() {
+           UsersTutorService.insertNewUsersTutor($routeParams.otherUserId,$scope.dati.userId,'Bottone disabilitato').then(function (ref) {
+               var followId = ref.key;
+               UsersTutorService.updateUsersTutor(tutorId);
+               $('#tutoredButton').attr('disabled',true);
+           });
+       };
+       $scope.removeTutor = function (tutorId) {
+           UsersTutorService.deleteTutor(tutorId);
+           $('#tutorButton').prop("disabled",false);
+       };
+
+       $scope.removeTutored = function (tutorId) {
+           UsersTutorService.deleteTutor(tutorId);
+           $('#tutoredButton').prop("disabled",false);
+       };
+   }]);
---
+++ app/components/users/user-tutor-service.js
@@ -0,0 +1,42 @@
+'use strict'
+
+angular.module('myApp.users.usersTutorService', [])
+
+    .factory('UsersTutorService', function usersTutorService($firebaseArray, $firebaseObject) {
+        var NewUsersTutorService = {
+            getTutors: function() {
+                var ref = firebase.database().ref().child("tutors");
+                return $firebaseArray(ref);
+            },
+
+            getUserInfo: function(userId) {
+                var userRef = firebase.database().ref().child("users").child(userId);
+                return $firebaseObject(userRef);
+            },
+            insertNewUsersTutored: function (tutor, tutored, button) {
+                // quando faccio richiesta per essere tutor io sono tutor e otherUser è tutored,
+                //quando faccio richiesta per avere un tutor io sono tutored e l'altro è tutor
+                var ref = firebase.database().ref().child("tutors");
+                // create a synchronized array
+                return $firebaseArray(ref).$add({
+                    tutor: tutor,
+                    tutored: tutored,
+                    button: button
+                });
+            },
+            updateUsersTutored: function (tutorId) {
+                //add the user to list of users and set the logged value to true
+                var ref = firebase.database().ref().child("tutors").child(tutorId);
+                // create a synchronized array
+                ref.update({
+                    id: tutorId
+                });
+            },
+            deleteTutor: function (tutorId) {
+                var refDel = firebase.database().ref().child("tutors").child(tutorId);
+                refDel.remove();
+            }
+        };
+        return NewUsersTutorService;
+    });
+
