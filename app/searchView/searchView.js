f52cc3b8fe080ad4cbb1ddda62ca0dea15c39343
MicheleDrago <s189127@studenti.polito.it>
2017-06-25 18:19:22 +0200

    sempre funzione search problema split() function

--- app/searchView/searchView.js
+++ app/searchView/searchView.js
@@ -2,7 +2,14 @@
 
 angular.module('myApp.searchView', ['ngRoute','myApp.forum','myApp.post','myApp.users','myApp.evento'])
 
-    .config(['$routeProvider', function($routeProvider) {
+   // .filter('split', function() {
+     //   return function(input, splitChar, splitIndex) {
+       //     // do some bounds checking here to ensure it has that index
+         //   return input.split(splitChar)[splitIndex];
+        //};
+    //})
+
+.config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/searchView/', {
             templateUrl: 'searchView/searchView.html',
             controller: 'searchViewCtrl',
@@ -19,17 +26,22 @@ angular.module('myApp.searchView', ['ngRoute','myApp.forum','myApp.post','myApp.
             }
         });
     }])
+
     //Inline Array Annotation
     //Here we pass an array whose elements consist of a list of strings (the names of the dependencies)
     // followed by the function itself.
     //When using this type of annotation, take care to keep the annotation array
     // in sync with the parameters in the function declaration.
     .controller('searchViewCtrl', ['$scope','$routeParams', 'Forum', 'Post', 'UserList', 'Evento',
-        function($scope, $routeParams, Forum, Post, UserList, Evento ) {
+        function($scope, $routeParams, Forum, Post, UserList, Evento) {
+
+
             //initialize variables
             $scope.dati = {};
             $scope.risultato={};
             $scope.risultati={};
+
+
 
             $scope.dati.forum = Forum.getData();
             console.log($scope.dati.forum);
@@ -39,31 +51,39 @@ angular.module('myApp.searchView', ['ngRoute','myApp.forum','myApp.post','myApp.
             console.log( $scope.dati.listaUtenti );
 
             $scope.dati.listaUtenti.$loaded().then(function () {
-               $scope.input= function () {
+                $scope.input = function () {
+                    $scope.dati.ricerca.split(" ");
+                    for (var i=0;i<$scope.dati.listaUtenti.length; i++){
+                                       $scope.nome=$scope.dati.listaUtenti[i].name;
+                                       $scope.nome.split(" ");
+                                       $scope.cognome=$scope.dati.listaUtenti[i].surname;
+                                       $scope.cognome.split(" ");
+                     for (var a=0, b=0, c=0;a<$scope.nome.length, b<$scope.cognome.length, c<$scope.dati.ricerca.length; a++ , b++, c++){
 
-                                   for (var i=0;i<$scope.dati.listaUtenti.length; i++){
-                        if ($scope.dati.ricerca === $scope.dati.listaUtenti[i].name){
+                        if (($scope.dati.ricerca[c] === $scope.nome[a])||
+                            ($scope.dati.ricerca[c] === $scope.cognome[b])||
+                            ($scope.dati.ricerca[c] === $scope.dati.listaUtenti[i].email))
+                        {
 
-                            $scope.risultato=$scope.dati.listaUtenti[i].name;
+                            $scope.risultato=$scope.dati.listaUtenti[i].name + ' '+
+                                $scope.dati.listaUtenti[i].surname + ' '+
+                                $scope.dati.listaUtenti[i].email ;
 
                         }
-                        if ($scope.dati.ricerca === $scope.dati.listaUtenti[i].surname){
 
-                            $scope.risultato=$scope.dati.listaUtenti[i].surname;
+                                   }
+                        console.log("$scope.dati.listaUtenti.length", $scope.dati.listaUtenti.length);
+                        console.log("numeri effettivi di cicli", i );}
+                                   console.log( $scope.dati.ricerca );
+                                   console.log( $scope.risultato);
 
-                        }
-                        if ($scope.dati.ricerca === $scope.dati.listaUtenti[i].email){
-
-                            $scope.risultato=$scope.dati.listaUtenti[i].email;
-
-                        }
-                                       console.log( $scope.dati.ricerca );
-
-                                   }}});
+                                   
+                    //return $scope.risultato;
+                }});
            $scope.dati.evento = Evento.getData();
            console.log( $scope.dati.listaUtenti );
 
             $scope.risultati=function(){
                 document.getElementById("contenitore").innerHTML = $scope.risultato;
             };
-        }]);
+                }]);
