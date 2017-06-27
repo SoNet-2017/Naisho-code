cfe717fef7595939df3ace7eca069ba1a5d132b5
MicheleDrago <s189127@studenti.polito.it>
2017-06-27 20:44:51 +0200

    carica tutto bene ma non mi fa il search

--- app/searchView/searchView.js
+++ app/searchView/searchView.js
@@ -40,12 +40,13 @@ angular.module('myApp.searchView', ['ngRoute','myApp.forum','myApp.post','myApp.
             $scope.dati = {};
             $scope.risultato={};
             $scope.risultati={};
-            //$scope.nome = {};
-           // $scope.cognome = {};
+            $scope.nome = {};
+            $scope.cognome = {};
 
             $scope.myID=currentAuth.uid;
 
-
+            $scope.dati.evento = Evento.getData();
+            console.log( $scope.dati.evento );
             $scope.dati.forum = Forum.getData();
             console.log($scope.dati.forum);
            $scope.dati.posts = Post.getData();
@@ -57,34 +58,37 @@ angular.module('myApp.searchView', ['ngRoute','myApp.forum','myApp.post','myApp.
                 $scope.dati.posts.$loaded().then(function () {
                 $scope.input = function () {
 
-                    //console.log($scope.dati.ricerca);
-                    //console.log("cosa sto cercando è un array di:",$scope.dati.ricerca.length);
-                    $scope.dati.ricerca.split(" ");
+
+                    $scope.dati.ricerca=$scope.dati.ricercaa.toLowerCase().split(" "); // ho dovuto fare cosi perchè altrimenti non si prende lo split
+                    console.log($scope.dati.ricerca);
                     for (var i = 0; i < $scope.dati.listaUtenti.length; i++) {
-                        $scope.nome = $scope.dati.listaUtenti[i].name;
-                        $scope.nome.split(" ");
-                        $scope.cognome = $scope.dati.listaUtenti[i].surname;
-                        $scope.cognome.split(" ");
+                        $scope.nome = $scope.dati.listaUtenti[i].name.toLowerCase().split(" ");
+
+                        $scope.cognome = $scope.dati.listaUtenti[i].surname.toLowerCase().split(" ");
+
                         console.log($scope.nome, $scope.cognome);
-                        for (var a = 0, b = 0, c = 0,d=0;
+                        for (var a = 0, b = 0, c = 0, d = 0;
                              a < $scope.nome.length, b < $scope.cognome.length, c < $scope.dati.ricerca.length, d < $scope.dati.posts.length;
                              a++ , b++, c++, d++) {
 
-                            if ((($scope.dati.ricerca[c] === $scope.nome[a]) ||
-                                ($scope.dati.ricerca[c] === $scope.cognome[b]) ||
-                                ($scope.dati.ricerca[c] === $scope.dati.listaUtenti[i].email)) || //c'è da vedere questo perchè con l'and se non pubblicano neanche un post allora non li trovi
-                                $scope.dati.posts[d].userPost===$scope.dati.listaUtenti[i].$id) {
+                            if (((String($scope.dati.ricerca[c]) === String($scope.nome[a])) ||
+                                (String($scope.dati.ricerca[c]) === String($scope.cognome[b])) ||
+                                (String($scope.dati.ricerca[c]) === String($scope.dati.listaUtenti[i].email))) && //c'è da vedere questo perchè con l'and se non pubblicano neanche un post allora non li trovi
+                                (String($scope.dati.posts[d].userPost) === String($scope.dati.listaUtenti[i].$id))) {
 
                                 $scope.risultato = $scope.dati.listaUtenti[i].name + ' ' +
                                     $scope.dati.listaUtenti[i].surname + ' ' +
                                     $scope.dati.listaUtenti[i].email;
-                                $scope.dati.userpost= $scope.dati.posts[d].userPost;
+
+                                $scope.dati.userpost=$scope.dati.posts[d].userPost;
+                                console.log( "cosa salva come risultato di ricerca:",$scope.risultato);
 
                             }
+                            //else {$scope.risultato=" ";}
 
                         }
-                        console.log("$scope.dati.listaUtenti.length", $scope.dati.listaUtenti.length);
-                        console.log("numeri effettivi di cicli", i);
+                        //console.log("$scope.dati.listaUtenti.length", $scope.dati.listaUtenti.length);
+                        //console.log("numeri effettivi di cicli", i);
                     }
                     console.log($scope.dati.ricerca);
                     console.log($scope.risultato);
@@ -95,13 +99,12 @@ angular.module('myApp.searchView', ['ngRoute','myApp.forum','myApp.post','myApp.
                 } })});
 
             //Azzero le variabili di ricerca
-            $scope.risultato={};
+            //$scope.risultato={};
             //$scope.nome={};
             //$scope.cognome={};
-           $scope.dati.evento = Evento.getData();
-           console.log( $scope.dati.listaUtenti );
 
-            $scope.risultati=function(){
-                document.getElementById("contenitore").innerHTML = $scope.risultato;
-            };
+
+           //$scope.risultati=function(){
+               // document.getElementById("contenitore").innerHTML = $scope.risultato;
+           // };
                 }]);
