dc77340f25f7446bc3ad9f125160434407212f86
Selene <s239776@studenti.polito.it>
2017-06-30 09:49:21 +0200

    funzione like

--- app/components/post/post-service.js
+++ app/components/post/post-service.js
@@ -14,7 +14,36 @@ angular.module('myApp.post.postService', [])
                 var ref = firebase.database().ref().child("commenti");
                 // download the data into a local object
                 return $firebaseArray(ref);
+            },
+            getLike: function () {
+                var ref = firebase.database().ref().child("likes");
+                return $firebaseArray(ref);
+            },
+
+            insertNewLike: function (postId, userId,  button) {
+                //add the critica to list of critucs and set the logged value to true
+                var ref = firebase.database().ref().child("likes");
+                // create a synchronized array
+                return $firebaseArray(ref).$add({
+                    postId: postId,
+                    userId: userId,
+                    button: button
+                });
+            },
+            updateLike: function (likeId) {
+                //add the user to list of users and set the logged value to true
+                var ref = firebase.database().ref().child("likes").child(likeId);
+                // create a synchronized array
+                ref.update({
+                    id: likeId
+                });
+            },
+            deleteLike: function (likeId) {
+                var refDel = firebase.database().ref().child("likes").child(likeId);
+                refDel.remove();
             }
+
+
         };
         return postService;
     });
