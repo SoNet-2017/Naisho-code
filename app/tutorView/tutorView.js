57542daa63181305859a7e47afda578e7077f5c6
Selene <s239776@studenti.polito.it>
2017-07-01 10:22:41 +0200

    tutor

--- app/tutorView/tutorView.js
+++ app/tutorView/tutorView.js
@@ -18,10 +18,73 @@ angular.module('myApp.tutorView', ['ngRoute'])
             }
         })
     }])
-    .controller('tutorViewCtrl', ['$scope', '$rootScope', '$routeParams', 'currentAuth',
-        function($scope, $rootScope, $routeParams, currentAuth){
+    .controller('tutorViewCtrl', ['$scope', '$rootScope', '$routeParams', 'currentAuth','UsersTutorsService','UsersFriends',
+        function($scope, $rootScope, $routeParams, currentAuth,UsersTutorsService,UsersFriends){
             //initialize variables
             $scope.dati = {};
+            $scope.amiciView=[];
+            $scope.tutorsView=[];
+            $scope.tutoredView=[];
+
+            //dati current user
+            $scope.dati.userId = UsersFriends.getUserInfo(currentAuth.uid);
+
+            // dati su tutte amicizie
+            $scope.dati.friends = UsersFriends.getFriends();
+            //console.log( $scope.dati.friends);
+            // dati su tutti i tutor
+            $scope.dati.tutors = UsersTutorsService.getTutors();
+            //console.log( $scope.dati.tutors);
+
+            $scope.dati.friends.$loaded().then(function(){
+                var friends = $scope.dati.friends;
+                for (var keySingleFriend in friends) {
+                    if (!angular.isFunction(keySingleFriend)) {
+                        if (!angular.isFunction(friends[keySingleFriend]))
+                        {
+                            if (friends[keySingleFriend]!=undefined && friends[keySingleFriend].friend1!=undefined && friends[keySingleFriend].friend2!=undefined) {
+                                if ($scope.dati.userId.$id == friends[keySingleFriend].friend1){
+                                    var persona=UsersFriends.getUserInfo(friends[keySingleFriend].friend2);
+                                    console.log(persona);
+                                    $scope.amiciView.push(persona);
+                                }
+                                else if( $scope.dati.userId.$id == friends[keySingleFriend].friend2){
+                                    var persona=UsersFriends.getUserInfo(friends[keySingleFriend].friend1);
+                                    //console.log(persona);
+                                    $scope.amiciView.push(persona);
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            $scope.dati.tutors.$loaded().then(function(){
+                var tutors = $scope.dati.tutors;
+                for (var keySingleTutor in tutors) {
+                    if (!angular.isFunction(keySingleTutor)) {
+                        if (!angular.isFunction(tutors[keySingleTutor]))
+                        {
+                            if (tutors[keySingleTutor]!=undefined && tutors[keySingleTutor].tutor!=undefined  && tutors[keySingleTutor].tutored!=undefined) {
+                                if ($scope.dati.userId.$id == tutors[keySingleTutor].tutor){
+                                    var persona=UsersFriends.getUserInfo(tutors[keySingleTutor].tutored);
+                                   // console.log(persona);
+                                    $scope.tutoredView.push(persona);
+                                    //$scope.tutored.push(tutors[keySingleTutor].tutored);
+                                    //console.log( $scope.tutored);
+                                }
+                                else if( $scope.dati.userId.$id == tutors[keySingleTutor].tutored){
+                                    var persona=UsersFriends.getUserInfo(tutors[keySingleTutor].tutor);
+                                    //console.log(persona);
+                                    $scope.tutorsView.push(persona);
+                                    //$scope.tutors.push(tutors[keySingleTutor].tutor);
+                                    //console.log( $scope.tutors);
+                                }
+                            }
+                        }
+                    }
+                }
+            });
 
 
         }]);
