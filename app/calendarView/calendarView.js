cc4b8bc0c6387a2e599c44f6afcee5546ce1f1cf
Selene <s239776@studenti.polito.it>
2017-07-06 11:59:08 +0200

    varie

--- app/calendarView/calendarView.js
+++ app/calendarView/calendarView.js
@@ -37,8 +37,8 @@ angular.module('myApp.calendarView', ['ngRoute','daypilot','myApp.evento','myApp
             if (String(b).length == 1) {
                 b = "0"+b;
             }
-var data= dato.getFullYear()+"-"+b+"-"+a;
-            console.log(data);
+            var data= dato.getFullYear()+"-"+b+"-"+a;
+           // console.log(data);
 
             var dp=document.getElementById("dp");
             console.log(dp);
@@ -61,37 +61,13 @@ var data= dato.getFullYear()+"-"+b+"-"+a;
                           end:f,
                           id: DayPilot.guid(),
                           text: $scope.eventi[i].title,
-                       // allday: true
+                          resource:'E'
                    });
-                    console.log(e.data);
-                    $scope.events.push(e.data);
+                    console.log(e);
+                    $scope.events.push(e);
                    console.log($scope.events);
                 }
             });
-            /*
-            $scope.add = function() {
-                $scope.events.push(
-                    {
-                        start: new DayPilot.Date("2014-09-01T10:00:00"),
-                        end: new DayPilot.Date("2014-09-01T12:00:00"),
-                        id: DayPilot.guid(),
-                        text: "Simple Event"
-                    }
-                );
-            };
 
-            $scope.move = function() {
-                var event = $scope.events[0];
-                event.start = event.start.addDays(1);
-                event.end = event.end.addDays(1);
-            };
 
-            $scope.rename = function() {
-                $scope.events[0].text = "New name";
-            };
-
-            $scope.message = function() {
-                $scope.dp.message("Hi");
-            };
- */
     }]);
