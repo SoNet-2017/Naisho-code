2ae2be7068128f4da903a6245138ecee1e7aed19
Selene <s239776@studenti.polito.it>
2017-07-10 11:15:00 +0200

    search

--- app/searchView/searchView.js
+++ app/searchView/searchView.js
@@ -40,6 +40,8 @@ angular.module('myApp.searchView', ['ngRoute','myApp.forum','myApp.post','myApp.
             $scope.dati = {};
             $scope.risultato={};
             $scope.risultati=[];
+            $scope.risultatiEventi=[];
+            $scope.risultatiForum=[];
             $scope.nome = {};
             $scope.cognome = {};
 
@@ -60,52 +62,65 @@ angular.module('myApp.searchView', ['ngRoute','myApp.forum','myApp.post','myApp.
             }
 
             $scope.dati.listaUtenti.$loaded().then(function () {
-                $scope.dati.posts.$loaded().then(function () {
-                $scope.cerca = function () {
-                    $scope.dati.ricerca=$scope.dati.ricercaa.toLowerCase().split(" "); // ho dovuto fare cosi perchè altrimenti non si prende lo split
-                    console.log($scope.dati.ricerca);
-                    for (var i = 0; i < $scope.dati.listaUtenti.length; i++) {
-                        $scope.nome = $scope.dati.listaUtenti[i].name.toLowerCase().split(" ");
-                        console.log($scope.nome);
-                        $scope.cognome = $scope.dati.listaUtenti[i].surname.toLowerCase().split(" ");
-                        console.log($scope.cognome);
-                        console.log($scope.nome, $scope.cognome);
-                        for (var c = 0; c < $scope.dati.ricerca.length;  c++) {
-                            if (((String($scope.dati.ricerca[c]) === String($scope.nome)) ||
-                                (String($scope.dati.ricerca[c]) === String($scope.cognome)) ||
-                                (String($scope.dati.ricerca[c]) === String($scope.dati.listaUtenti[i].email))) //c'è da vedere questo perchè con l'and se non pubblicano neanche un post allora non li trovi
-                                ) {
-                                var risultato = $scope.dati.listaUtenti[i].name + ' ' +$scope.dati.listaUtenti[i].surname + ' ' +  $scope.dati.listaUtenti[i].email;
-                                var id=$scope.dati.listaUtenti[i].$id;
-                                console.log( "cosa salva come risultato di ricerca:",risultato, id);
-                                $scope.risultati.push({risultato:risultato, id:id});
+                $scope.dati.evento.$loaded().then(function ()     {
+                        $scope.dati.forum.$loaded().then(function () {
 
+                            $scope.cerca = function () {
+                                $scope.dati.ricerca = $scope.dati.ricercaa.toLowerCase().split(" ");
+                                // ho dovuto fare cosi perchè altrimenti non si prende lo split
+                                console.log($scope.dati.ricerca);
+                                for (var i = 0; i < $scope.dati.listaUtenti.length; i++) {
+                                    $scope.nome = $scope.dati.listaUtenti[i].name.toLowerCase().split(" ");
+                                    //console.log($scope.nome);
+                                    $scope.cognome = $scope.dati.listaUtenti[i].surname.toLowerCase().split(" ");
+                                    //console.log($scope.cognome);
+                                    //console.log($scope.nome, $scope.cognome);
+                                    for (var c = 0; c < $scope.dati.ricerca.length; c++) {
+                                        if (String($scope.dati.ricerca[c]) === String($scope.nome) ||
+                                            String($scope.dati.ricerca[c]) === String($scope.cognome) ||
+                                            String($scope.dati.ricerca[c]) === String($scope.dati.listaUtenti[i].email)) {
+                                            var risultato = $scope.dati.listaUtenti[i].name + ' ' + $scope.dati.listaUtenti[i].surname + ' ' + $scope.dati.listaUtenti[i].email;
+                                            var id = $scope.dati.listaUtenti[i].$id;
+                                            console.log("cosa salva come risultato di ricerca:", risultato, id);
+                                            $scope.risultati.push({risultato: risultato, id: id});
+                                        }
+                                        //else {$scope.risultato=" ";}
+
+                                    }
+                                   // console.log($scope.dati.ricerca);
+                                }
+                                for (var i = 0; i < $scope.dati.evento.length; i++) {
+                                    $scope.titolo = $scope.dati.evento[i].title.toLowerCase().split(" ");
+                                    console.log($scope.titolo);
+                                    console.log($scope.dati.ricerca.length);
+                                    for (var c = 0; c < $scope.dati.ricerca.length; c++) {
+                                        for (var j = 0; j < $scope.titolo.length; j++) {
+                                            if (String($scope.dati.ricerca[c]) === String($scope.titolo[j])) {
+                                                var risultato = $scope.dati.evento[i].title + ' ' + $scope.dati.evento[i].Citta + ' ' + $scope.dati.evento[i].start;
+                                                var id = $scope.dati.evento[i].$id;
+                                                console.log("cosa salva come risultato di ricerca:", risultato, id);
+                                                $scope.risultatiEventi.push({risultato: risultato, id: id});
+                                            }
+                                        }
+                                    }
+                                }
+                                for (var i = 0; i < $scope.dati.forum.length; i++) {
+                                    $scope.titolo = $scope.dati.forum[i].titolo.toLowerCase().split(" ");
+                                    console.log($scope.titolo);
+                                    for (var c = 0; c < $scope.dati.ricerca.length; c++) {
+                                        for (var j = 0; j < $scope.titolo.length; j++) {
+                                            if (String($scope.dati.ricerca[c]) === String($scope.titolo[j])) {
+                                                var risultato = $scope.dati.forum[i].titolo;
+                                                var id = $scope.dati.forum[i].$id;
+                                                console.log("cosa salva come risultato di ricerca:", risultato, id);
+                                                $scope.risultatiForum.push({risultato: risultato, id: id});
+                                            }
+                                        }
+                                    }
+                                }
+                                if ($scope.risultati.length<=0 && $scope.risultatiEventi.length<=0 && $scope.risultatiForum.length<=0) $scope.fine = true;
                             }
-                            //else {$scope.risultato=" ";}
-
-                        }
-                        //console.log("$scope.dati.listaUtenti.length", $scope.dati.listaUtenti.length);
-                        //console.log("numeri effettivi di cicli", i);
-                    }
-                    console.log($scope.dati.ricerca);
-                    console.log($scope.risultato);
-
-
-
-                    //return $scope.risultato;
-        }
-                if ($scope.risultati.length<=0) $scope.fine=true;
-                }
-                )}
-                );
-
-            //Azzero le variabili di ricerca
-            //$scope.risultato={};
-            //$scope.nome={};
-            //$scope.cognome={};
-
-
-           //$scope.risultati=function(){
-               // document.getElementById("contenitore").innerHTML = $scope.risultato;
-           // };
-                }]);
+                        })
+                })
+            });
+         }]);
