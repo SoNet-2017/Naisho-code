83456f3317532457309d5ec21b54bdd079f6264f
Selene <s239776@studenti.polito.it>
2017-07-12 16:57:52 +0200

    css

--- app/changeImageView/changeImageView.js
+++ app/changeImageView/changeImageView.js
@@ -63,6 +63,7 @@ angular.module('myApp.changeImageView', ['ngRoute','myApp.users'])
 
     $scope.finalChange = function()
     {
+        //noinspection JSUnresolvedFunction
         EditProfileService.cambiaImg( $scope.dati.userId, $scope.imgPath).$loaded().then(function() {
 
             Users.updateUser($scope.dati.userId);
